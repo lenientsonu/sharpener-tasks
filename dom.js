809dd53c4cc8d9649ce496df0aa032eb6304e6f8
Sonu <113425856+lenientsonu@users.noreply.github.com>
2022-12-22 22:54:02 +0530

    Add files via upload
    Created 2 Nodes and Modified Dom in dom.js according to task 7

--- dom.js
+++ dom.js
@@ -54,12 +54,75 @@
 
 //code for queryselectorall
 
-let li = document.querySelectorAll('.list-group-item');
-li[1].style.color = 'green';
+// let li = document.querySelectorAll('.list-group-item');
+// li[1].style.color = 'green';
 
-let odd = document.querySelectorAll('li:nth-child(odd)');
+// let odd = document.querySelectorAll('li:nth-child(odd)');
 
-for (let i of odd){
-	i.style.backgroundColor = 'green';
-}
+// for (let i of odd){
+// 	i.style.backgroundColor = 'green';
+// }
 
+
+//traversing the dom //
+
+//let itemList = document.querySelector('#items');
+
+//parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+//parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+
+//childNodes
+// console.log(itemList.childNodes);
+
+//children
+// console.log(itemList.children[1]);
+
+//firstChild & lastChild
+// console.log(itemList.firstChild);
+
+//firstElementChild & lastElementChild
+// console.log(itemList.firstElementChild);
+
+// //nextSibling & nextElementSibling
+// console.log(itemList.nextSibling);
+// console.log(itemList.nextElementSibling);
+
+// //previousSibling & previousElementSibling
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+
+//createElement
+let newDiv = document.createElement('div');
+
+//add class
+newDiv.className = 'hello';
+
+//add id
+newDiv.id = 'hello1';
+
+//add attr
+newDiv.setAttribute('title','Hello div');
+
+//create text node
+let newDivText = document.createTextNode('Hello World');
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+// console.log(newDiv);
+
+//inserting hello world before Item Lister
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+//inserting hello world before Item 1
+let ul = document.querySelector('ul');
+let li = document.querySelector('li');
+ul.insertBefore(newDiv,li);
