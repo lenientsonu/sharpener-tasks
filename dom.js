d0c59e3a754bf997792c7e62b911e73f48719651
lenientsonu <lenientsonu@gmail.com>
2022-12-22 16:30:41 +0530

    modified dom.js for task 4

--- dom.js
+++ dom.js
@@ -1,14 +1,23 @@
-document.title = 'Title changed';
+// document.title = 'Title changed';
 
-let headerTitle = document.getElementById('header-title');
+// let headerTitle = document.getElementById('header-title');
 
-console.log(headerTitle.textContent);
+// console.log(headerTitle.textContent);
 
-headerTitle.style.borderBottom = 'solid 3px #000';
+// headerTitle.style.borderBottom = 'solid 3px #000';
 
-let addTitle = document.querySelector('.title');
+// let addTitle = document.querySelector('.title');
 
-addTitle.style = 'font-weight:bold';
+// addTitle.style.fontWeight = 'bold';
 
+// addTitle.style = 'color:green';
 
-addTitle.style = 'color:green';
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[2].style.backgroundColor = 'green';
+
+for(let i of items)
+{
+	i.style.fontWeight = 'bold';
+}
