f840b077cbc5233db1aef388ff8cdaa0074d9a3c
lenientsonu <lenientsonu@gmail.com>
2022-12-22 16:47:44 +0530

    updated dom.js for task 5

--- dom.js
+++ dom.js
@@ -12,12 +12,24 @@
 
 // addTitle.style = 'color:green';
 
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+
+// items[2].style.backgroundColor = 'green';
+
+// for(let i of items)
+// {
+// 	i.style.fontWeight = 'bold';
+// }
+
+
 let items = document.getElementsByClassName('list-group-item');
-console.log(items);
 
-items[2].style.backgroundColor = 'green';
+items[3].style.color = 'red';
 
-for(let i of items)
-{
-	i.style.fontWeight = 'bold';
-}
+//gives error because new li does not have classname
+//items[4].style.color = 'red';
+
+let li = document.getElementsByTagName('li');
+
+li[4].style.color = 'red';
