381a63bef0241d8e1aa1fecfca307fb44341d05d
lenientsonu <lenientsonu@gmail.com>
2022-12-22 16:06:24 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+document.title = 'Title changed';
+
+let headerTitle = document.getElementById('header-title');
+
+console.log(headerTitle.textContent);
+
+headerTitle.style.borderBottom = 'solid 3px #000';
+
+let addTitle = document.querySelector('.title');
+
+addTitle.style = 'font-weight:bold';
+
+
+addTitle.style = 'color:green';
