67ef00aac1e37d437cbd9b336fd75e404a82e718
Sonu <113425856+lenientsonu@users.noreply.github.com>
2022-12-23 16:49:42 +0530

    task 11 Lets store the users in the right format
    features added
    -created a object with all user input, serialized the object to string format and stored that into local storage

--- task-10.js
+++ task-10.js
@@ -16,12 +16,24 @@ function onbuttonclick(event)
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
     const passwd = document.getElementById('passwd').value;
-    console.log(name,email,phone,passwd);
+    //creating a object of user details from input
+    let userDetails = {
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'passwd': passwd
+    };
+    //converting the userDetails object into string
+    let userDetails_serialized = JSON.stringify(userDetails);
+    
     //setting the constant values in local storage
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
-    localStorage.setItem('phone',phone);
-    localStorage.setItem('passwd',passwd);
+    // localStorage.setItem('name',name);
+    // localStorage.setItem('email',email);
+    // localStorage.setItem('phone',phone);
+    // localStorage.setItem('passwd',passwd);
+    //setting the user details int the local storage as an object
+    localStorage.setItem('userDetails',userDetails_serialized);
+    
     //setting the const/input values in seesion storage
     // sessionStorage.setItem('name',name);
     // sessionStorage.setItem('email',email);
