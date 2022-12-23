16723a917e3a2cc8acde8a2bc469097d6b362e8f
Sonu <113425856+lenientsonu@users.noreply.github.com>
2022-12-23 15:12:39 +0530

    Task 9 Filter
    -Filter functionality added 
    -another input of description added and updated filter to filter through description as well

--- task-8.js
+++ task-8.js
@@ -1,11 +1,14 @@
 //get the form and ul(list) in variables
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
 
 //form submit event => add a list item of input of form
 form.addEventListener('submit', addItem);
 //delete button event => delete the list item whose button is clicked
 itemList.addEventListener('click', removeItem);
+//searchbar(filter) event
+filter.addEventListener('keyup', filterItems);
 
 //function addItem for form submit event to add item in list
 function addItem(e){
@@ -18,6 +21,10 @@ function addItem(e){
     li.className = 'list-group-item';
     //add text node with input value(newItem)
     li.appendChild(document.createTextNode(newItem));
+    //get desc value
+    let descInput = document.getElementById('desc').value;
+    //add desc text node to new li
+    li.appendChild(document.createTextNode("\n"+descInput));
     //create delete button & add class to that delete button
     let deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
@@ -46,3 +53,26 @@ function removeItem(e){
         }
     }    
 }
+
+
+//function to filter items from itemList
+function filterItems(e){
+
+    //get text from filter search bar & convert to lowercase
+    let filterText = e.target.value.toLowerCase();
+    //getting the List items(Lis) from listItem(ul)
+    let items = itemList.getElementsByTagName('li');
+    //convert the items to an array & iterate the array to match the filter text
+    Array.from(items).forEach(function(item){
+        let itemName = item.childNodes[0].textContent;
+        let descName = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(filterText) != -1 || descName.toLowerCase().indexOf(filterText) != -1){
+            item.style.display = 'block';
+        }
+        else{
+            item.style.display = 'none';
+        }
+    })
+}
+
+
