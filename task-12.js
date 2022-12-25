f01e4a95a429cea6163af1139f5a9d8539b3a8db
Sonu <113425856+lenientsonu@users.noreply.github.com>
2022-12-25 14:40:50 +0530

    Task 13 Delete/Edit data from Local Storage
    Features Added:
    -added delete and edit buttons and their functionalities
    -checks if the email id is already registered if yes then replaces the old with new one

--- task-12.js
+++ task-12.js
@@ -43,7 +43,6 @@ function onbuttonclick(event)
     
     //setting the user details into the local storage as an object
     localStorage.setItem(userDetails.email,userDetails_serialized);
-    
     //calling addUserToScreen func to add the new user object and display on screen
     addUserToScreen(userDetails);
     
@@ -52,24 +51,54 @@ function onbuttonclick(event)
 
 function addUserToScreen(object){
 
+    //checking if the email exist already
+    if(localStorage.getItem(object.email) !== null){
+        //console.log('exists');
+        removeUser(object.email);
+    }
+
     //getting the parent ul & creating a child li
     const ul = document.getElementById('users-list');
     const li = document.createElement('li');
     //adding user details to li 
     li.appendChild(document.createTextNode(object.name+' => '+object.email));
+    //adding id to li
+    li.id = object.email;
+    // console.log(li.id);
     //creating delete button & adding to the new li element
     const delBtn = document.createElement('button');
     delBtn.className = 'btn btn-danger btn-sm float-right delete';
-    delBtn.appendChild(document.createTextNode('X'));
+    delBtn.appendChild(document.createTextNode('Delete User'));
     li.appendChild(delBtn);
     //creating edit button and addig to new li element
     const editBtn = document.createElement('button');
     editBtn.className = 'btn btn-info btn-sm float-right edit';
     editBtn.appendChild(document.createTextNode('edit'));
     li.appendChild(editBtn);
-    
-    //console.log(li);
+    //adding the created li to ul
     ul.appendChild(li);
+
+    //delete the user(li) from userlist(ul) & from local storage 
+    delBtn.addEventListener('click', (e) => {
+        localStorage.removeItem(object.email);
+        li.remove();
+    });
+
+    editBtn.addEventListener('click', (e) => {
+        document.getElementById('name').value = object.name;
+        document.getElementById('email').value = object.email;
+        document.getElementById('phone').value = object.phone;
+        document.getElementById('passwd').value = object.passwd;
+        li.remove();
+    });
 }
 
-
+function removeUser(email){
+    
+    const parentNode = document.getElementById('users-list');
+    const childNodeToBeDeleted = document.getElementById(email);
+    if(childNodeToBeDeleted){
+        //console.log(childNodeToBeDeleted);
+        parentNode.removeChild(childNodeToBeDeleted);
+    }
+}
