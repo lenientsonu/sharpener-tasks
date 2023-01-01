f92c3087205ce47b3b42042f8bc26d1572c2c4c7
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-01-01 20:57:29 +0530

    Promises

---
+++ promises.js
@@ -0,0 +1,70 @@
+const posts = [
+    {title: 'Post One' , body:'This is post one'},
+    {title: 'Post Two' , body:'This is post two'}
+];
+
+
+function getPosts(){
+    
+    // setTimeout(()=>{
+        let output = '';
+        posts.forEach((post,index) => {
+            output += `<li>${post.title}</li>`
+        });
+        document.body.innerHTML = output;
+    // }, 1000);
+
+}
+
+function createPost(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.push(post);
+            const err = false;
+            if(!err){
+                resolve();
+            }
+            else{
+                reject('Error: Something went wrong');
+            }
+        },1000);
+    });    
+};
+
+function deletePost(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(posts.length){
+                resolve(posts.pop());
+            }
+            else{
+                reject('Array Is Empty Now');
+            }
+        },1000);
+    });
+    
+};
+
+createPost({title:'Post Three', body:'This is post three'})
+.then(()=>{
+    getPosts();
+    deletePost().then(()=>{
+        getPosts();
+        deletePost().then(()=>{
+            getPosts();
+            deletePost().then(()=>{
+                getPosts();
+                createPost({title:'Post Four', body:'This is post four'}).then(()=>{
+                    getPosts();
+                    deletePost().then(getPosts).catch(err => console.log(err));
+                }).catch(err => console.log(err));
+            }).catch(err => console.log(err));
+        }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
+}).catch(err => console.log(err));
+
+
+// deletePost().then(getPosts).catch(err => console.log(err));
+
+
+
