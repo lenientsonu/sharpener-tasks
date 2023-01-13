24931c46cb962c22778e28bda5adccc75c03fa88
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-01-13 21:51:01 +0530

    task 9 of Get started with React
    showing locationOfExpenditure

---
+++ ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  
+  return (
+    <div>
+      <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+        </div>
+        <div className="expense-item__description">{props.locationOfExpenditure}</div>
+        <div className="expense-item__price">Rs. {props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
