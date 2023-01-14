a9dab37620d3f6be011d5bbbbfc1d910313bca86
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-01-14 23:00:23 +0530

    Task 12

--- react_started/ExpenseItem.js
+++ react_started/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails";
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <div>
       <div className="expense-item">
         <ExpenseDate date={props.date} />
-        <ExpenseDetails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={props.amount} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+        </div>
+        <div className="expense-item__description">
+          {props.locationOfExpenditure}
+        </div>
+        <div className="expense-item__price">Rs. {props.amount}</div>
       </div>
     </div>
   );
