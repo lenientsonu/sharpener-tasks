3b317e1e90663e6df6e61ccf1e034143d6c9d335
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-01-15 18:38:25 +0530

    Advance React Task 2
    implemented delete button with onClick event and eventHandler function. code is still incomplete for deleting ExpenseItem component

--- react_started/ExpenseItem.js
+++ react_started/ExpenseItem.js
@@ -1,18 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    //code to delete the ExpenseItem
+  }
+
   return (
     <div>
-      <div className="expense-item">
+      <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
         </div>
-        <div className="expense-item__description">
-          {props.locationOfExpenditure}
-        </div>
         <div className="expense-item__price">Rs. {props.amount}</div>
-      </div>
+        <button onClick={clickHandler}>Delete</button>
+      </Card>
     </div>
   );
 }
