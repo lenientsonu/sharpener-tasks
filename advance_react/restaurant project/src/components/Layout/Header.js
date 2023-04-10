bdbf7939bd9c2029f1fc2b77403aa3e9a88dfc1b
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-04-11 00:34:28 +0530

    some minor changes
    - used react fragment as wrapper
    - added image
    - added icon in cart button (svg)
    - added badge in cart button

--- advance_react/restaurant project/src/components/Layout/Header.js
+++ advance_react/restaurant project/src/components/Layout/Header.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import HeaderCartButton from './HeaderCartButton'
-import './Header.css'
-import './HeaderCartButton.css'
+import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import "./Header.css";
+import "./HeaderCartButton.css";
+import mealsImage from "./meals.jpg";
 
 const Header = () => {
     return (
-        <div className='header'>
-            <h1>Little Lemon</h1>
-            <HeaderCartButton />
-        </div>
-    )
-}
+        <React.Fragment>
+            <header className='header'>
+                <h1>Little Lemon</h1>
+                <HeaderCartButton />
+            </header>
+            <div className='main-image'>
+                <img src={mealsImage} alt='Header' />
+            </div>
+        </React.Fragment>
+    );
+};
 
-export default Header
+export default Header;
--- advance_react/restaurant project/src/components/Layout/HeaderCartButton.js
+++ advance_react/restaurant project/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,17 @@
 import React from "react";
+import CartIcon from "../Cart/CartIcon";
 import "./HeaderCartButton.css";
 
 const HeaderCartButton = () => {
-    return <button className='button'> Your Cart</button>;
+    return (
+        <button className='button'>
+            <span className='icon'>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className='badge'>0</span>
+        </button>
+    );
 };
 
 export default HeaderCartButton;
