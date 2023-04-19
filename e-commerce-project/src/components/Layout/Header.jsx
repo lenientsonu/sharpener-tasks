41577d5cd05e5ce56fa26881a9b8680b4db3862f
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-04-20 00:02:20 +0530

    Added Routes for about page
    - Added Routes for about page
    - Added about page component

--- e-commerce-project/src/components/Layout/Header.jsx
+++ e-commerce-project/src/components/Layout/Header.jsx
@@ -9,13 +9,13 @@ import './Header.css'
 const Header = () => {
     return (
         <>
-        <Navbar bg='dark' variant='dark'>
+        <Navbar bg='dark' variant='dark' sticky="top">
             <Container>
                 {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
                 <Nav className='me-auto'>
-                    <Nav.Link href='#home'>Home</Nav.Link>
+                    <Nav.Link href='/'>Home</Nav.Link>
                     <Nav.Link href='#store'>Store</Nav.Link>
-                    <Nav.Link href='#about'>About</Nav.Link>
+                    <Nav.Link href='/about'>About</Nav.Link>
                 </Nav>
                 <HeaderCartButton />
             </Container>
---
+++ e-commerce-project/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import Header from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer";
+import Products from "../components/Products/Products";
+import Cart from "../components/Cart/Cart";
+import CartProvider from "../store/CartProvider";
+
+import "./Home.css";
+
+const HomePage = () => {
+    return (
+        <CartProvider>
+            <>
+                <Header />
+                <Products />
+                <Cart title={"See the Cart"} color={"primary"} />
+                <Footer />
+            </>
+        </CartProvider>
+    );
+};
+
+export default HomePage;
