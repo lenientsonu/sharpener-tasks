35049fd1f65048d5e17ba6db86563a64a13c5821
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-04-29 00:45:08 +0530

    Task 10: Adding new Movies into the list
    Task 10: Adding new Movies into the list

---
+++ star-wars/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+
+function App() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchMoviesHandler = useCallback(async () => {
+        setError(null);
+        setIsLoading(true);
+
+        try {
+            const response = await fetch("https://swapi.dev/api/films/");
+            console.log("request fetched");
+
+            if (!response.ok) {
+                throw new Error("Something went wrong... Reloading");
+            }
+
+            const data = await response.json();
+            const transformedMovies = data.results.map((movieData) => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date,
+                };
+            });
+            setMovies(transformedMovies);
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }, []);
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, []);
+
+    let content = <p>Loading...</p>;
+    if (!isLoading && movies.length > 0) {
+        content = <MoviesList movies={movies} />;
+    }
+    if (!isLoading && movies.length === 0 && !error) {
+        content = <p>No Movies Found... </p>;
+    }
+    if (!isLoading && error) {
+        content = <p>{error}</p>;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <AddMovie />
+            </section>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ star-wars/src/components/AddMovie.js
@@ -0,0 +1,34 @@
+import "./AddMovie.css";
+
+const AddMovie = () => {
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        const movie = {
+            id: Math.random().toString(),
+            title: document.getElementById("title").value,
+            openingText: document.getElementById("text").value,
+            releaseDate: document.getElementById("date").value,
+        };
+        console.log(movie);
+    };
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div className='control'>
+                <label htmlFor='title'>Title</label>
+                <input type='text' id='title' />
+            </div>
+            <div className='control'>
+                <label htmlFor='text'>Opening Text</label>
+                <input type='text' id='text' />
+            </div>
+            <div className='control'>
+                <label htmlFor='date'>Release Date</label>
+                <input type='date' id='date' />
+            </div>
+            <button>Add Movie</button>
+        </form>
+    );
+};
+
+export default AddMovie;
