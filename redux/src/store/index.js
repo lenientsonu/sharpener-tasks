d2b1a505307dedec944c6154ae41713211940c85
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-05-26 17:48:45 +0530

    redux demo last update

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,46 +1,13 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-    name: "counter",
-    initialState: initialCounterState,
-    reducers: {
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        decrease(state, action) {
-            state.counter = state.counter - action.payload;
-        },
-        toggle(state) {
-            state.showCounter = !state.showCounter;
-        },
-    },
-});
-
-const initialAuthState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-    name: "auth",
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-        },
-    },
-});
+import counterReducer from "./counterSlice";
+import authReducer from "./authSlice";
 
 const store = configureStore({
     reducer: {
-        counter: counterSlice.reducer,
-        auth: authSlice.reducer,
+        counter: counterReducer,
+        auth: authReducer,
     },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
