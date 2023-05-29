e7bfddb86eac3e3cadd42d5113e0beb8e02a7a77
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-05-29 14:56:42 +0530

    Task 20: Doing Network calls and andling response the right way

--- advance-redux/src/App.js
+++ advance-redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,14 @@ import Products from "./components/Shop/Products";
 
 function App() {
     const showCart = useSelector((state) => state.cart.showCart);
+    const items = useSelector((state) => state.items);
+
+    useEffect(() => {
+        fetch(
+            "https://cart-5876f-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+            { method: "PUT", body: JSON.stringify(items) }
+        );
+    }, [items]);
 
     return (
         <Layout>
